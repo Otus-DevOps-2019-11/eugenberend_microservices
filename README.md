10180de031c57a6dc67151357646c83d055d087e
Evgeniy Berendyaev <evgeniy.berendyaev@gmail.com>
2020-02-27 17:05:12 +0300

    Added compose override

--- README.md
+++ README.md
@@ -11,3 +11,5 @@ Answers:
 2020/02/27 07:18:06 [emerg] 1#1: bind() to 0.0.0.0:80 failed (98: Address already in use)
 nginx: [emerg] bind() to 0.0.0.0:80 failed (98: Address already in use)
 ```
+
+- [X] ```docker-compose``` assigns a prefix for resources. If it isn't explicitly specified using ```-p``` arg, it defaults to the directory name
--- src/build-docker-images.sh
+++ src/build-docker-images.sh
@@ -1,3 +1,7 @@
 docker build -t evgeniyberendyaev/post:1.0 ./post-py -f ./post-py/Dockerfile.1
 docker build -t evgeniyberendyaev/comment:1.0 ./comment -f ./comment/Dockerfile.1
 docker build -t evgeniyberendyaev/ui:1.0 ./ui -f ./ui/Dockerfile.1
+
+#docker build -t evgeniyberendyaev/post:1.0 ./post-py
+#docker build -t evgeniyberendyaev/comment:1.0 ./comment
+#docker build -t evgeniyberendyaev/ui:1.0 ./ui
