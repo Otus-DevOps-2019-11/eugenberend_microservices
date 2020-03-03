735be36e83a271e0d308931fa999ab4cc6c2d519
Evgeniy Berendyaev <evgeniy.berendyaev@gmail.com>
2020-03-03 14:37:26 +0300

    Configured prometheus and node exporter as services

--- monitoring/create-fw-rules.sh
+++
@@ -1,2 +0,0 @@
-gcloud compute firewall-rules create prometheus-default --allow tcp:9090
-gcloud compute firewall-rules create puma-default --allow tcp:9292
--- monitoring/deploy-prometheus.sh
+++
@@ -1,1 +0,0 @@
-docker run --rm -p 9090:9090 -d --name prometheus prom/prometheus:v2.1.0
